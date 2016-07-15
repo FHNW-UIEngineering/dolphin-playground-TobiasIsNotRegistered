34209745a9d22d3348744dd56cda50392bb8fe3f
Dierk Koenig <dierk.koenig@canoo.com>
2016-07-15 17:57:56 +0200

    minor: rename service call

--- shared/src/main/java/myapp/MyAppCommands.java
+++ shared/src/main/java/myapp/MyAppCommands.java
@@ -4,7 +4,7 @@ package myapp;
  * @author Dieter Holz
  */
 public interface MyAppCommands {
-	String CMD_CALL_MY_SERVICE = unique("LogOnServer");
+	String CMD_CALL_MY_SERVICE = unique("CallMyService");
 
 	static String unique(String key) {
 		return MyAppCommands.class.getName() + "." + key;
